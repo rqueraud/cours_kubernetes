afe0ac5edf278c916089280957220f833601c316
Simon DUCAMP <ducampsimon@outlook.fr>
2024-12-15 14:14:04 +0100

    deploy: delegated to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-kubectl create namespace airflow
+kubectl create namespace airflow || true
+docker build -t 2024_kubernetes_post_pusher -f ./post_pusher/Dockerfile .
+docker build -t 2024_kubernetes_post_consumer -f ./post_consumer/Dockerfile .
 
 kubectl apply -f ./airflow/airflow-dags-pv.yaml 
 kubectl apply -f ./airflow/airflow-dags-pvc.yaml 
@@ -9,7 +11,7 @@ helm repo add airflow-stable https://airflow-helm.github.io/charts
 helm install airflow airflow-stable/airflow --namespace airflow --version 8.9.0 --values ./airflow/custom-values.yaml 
 kubectl port-forward svc/airflow-web 8080:8080 --namespace airflow
 
-kind delete cluster
+kind delete cluster || true
 kind create cluster --config ./kind/config.yaml
 
 kind load docker-image 2024_kubernetes_post_pusher
