12b7ec0c47ae153f6afcbcd9c1b5c6973250b31c
Simon DUCAMP <ducampsimon@outlook.fr>
2024-12-15 14:07:12 +0100

    deploy: remove docker run in CI/CD

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,6 @@
-# TP Kubernetes - Cours Data DevOps - Enseirb-Matmeca
-
-Installer git-lfs avant de cloner le repo pour pouvoir telecharger le fichier de données :
-
-```bash
-brew install git-lfs
-git lfs install
-git clone git@github.com:rqueraud/cours_kubernetes.git
-```
-
-Placez le fichier `service-account.json` à la racine du projet.
-
-Pour builder les images : 
-```bash
-docker build -t 2024_kubernetes_post_pusher -f ./post_pusher/Dockerfile .
-docker build -t 2024_kubernetes_post_consumer -f ./post_consumer/Dockerfile .
-```
-
-Pour executer les images :
-```bash
-docker run 2024_kubernetes_post_pusher
-```
-
-## Lancer le déploiement
-
-```bash
-git pull origin main
+#!/bin/bash
 
 kubectl create namespace airflow
-
-docker build -t 2024_kubernetes_post_pusher -f ./post_pusher/Dockerfile .
-docker build -t 2024_kubernetes_post_consumer -f ./post_consumer/Dockerfile .
 
 kubectl apply -f ./airflow/airflow-dags-pv.yaml 
 kubectl apply -f ./airflow/airflow-dags-pvc.yaml 
@@ -53,4 +24,3 @@ kubectl apply -f cours_kafka/ui/deployment.yaml
 
 kubectl apply -f post_pusher/deployment.yaml 
 kubectl apply -f post_consumer/deployment.yaml 
-```
