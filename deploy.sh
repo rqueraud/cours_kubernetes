ca503b6ad8d64aa5fb801ac7946da94fdf1d6e63
Simon DUCAMP <ducampsimon@outlook.fr>
2024-12-17 09:50:35 +0100

    trying sh instead of bash

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Creating namespaces..."
 kubectl create namespace airflow || true
@@ -18,8 +18,8 @@ kind delete cluster || true
 kind create cluster --config ./kind/config.yaml
 
 echo "Loading Docker images into Kind..."
-sudo kind load docker-image 2024_kubernetes_post_pusher
-sudp kind load docker-image 2024_kubernetes_post_consumer  
+kind load docker-image 2024_kubernetes_post_pusher
+kind load docker-image 2024_kubernetes_post_consumer  
 
 echo "Deploying Kafka..."
 kubectl apply -f cours_kafka/kafka/deployment.yaml 
